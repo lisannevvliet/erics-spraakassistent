c0d3be08f384912c3cb2ccfca9c4d6dd5479878a
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-20 14:54:43 +0200

    Implemented copy and paste functionality

--- script.js
+++ script.js
@@ -3,38 +3,49 @@ function $(element) {
     return document.querySelector(element)
 }
 
-// https://blog.zolomohan.com/speech-recognition-in-javascript
 // Check if speech recognition is supported. If not, log an error message.
+// https://blog.zolomohan.com/speech-recognition-in-javascript
 if ("webkitSpeechRecognition" in window) {
     // Initialize webkitSpeechRecognition.
     let speechRecognition = new webkitSpeechRecognition()
     // String for the final transcript.
     let final_transcript = ""
 
-    // Manually stop the recording, instead of automatically.
-    speechRecognition.continuous = true
+    // Automatically stop the recording.
+    speechRecognition.continuous = false
     // Enable interim results.
     speechRecognition.interimResults = true
     // Set the language to Dutch.
     speechRecognition.lang = "nl-NL"
 
+    // Boolean for the state of the speech recognition.
+    let active = false
+
     speechRecognition.onstart = () => {
-        // Show the status.
-        $("#status").style.display = "block"
+        // Set the state to active.
+        active = true
+
+        // Darken the record button.
+        $("#record").classList.add("dark")
     }
 
     speechRecognition.onerror = () => {
-        // Hide the status.
-        $("#status").style.display = "none"
+        // Set the state to inactive.
+        active = false
+
+        // Lighten the record button.
+        $("#record").classList.remove("dark")
+
         // Log an error message.
         console.log("Spraakherkenningsfout.")
     }
 
     speechRecognition.onend = () => {
-        // Hide the status.
-        $("#status").style.display = "none"
+        // Set the state to inactive.
+        active = false
 
-        console.log("Spraakherkenning beëindigd.")
+        // Lighten the record button.
+        $("#record").classList.remove("dark")
     }
 
     speechRecognition.onresult = (event) => {
@@ -46,6 +57,55 @@ if ("webkitSpeechRecognition" in window) {
             // Add the result to the corresponding string (final or interim).
             if (event.results[index].isFinal) {
                 final_transcript += event.results[index][0].transcript
+
+                // Check if the word "kopieer" is said.
+                // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard#reading_from_the_clipboard
+                if (event.results[index][0].transcript == "kopieer") {
+                    // String for the selected text.
+                    var text = ""
+                    
+                    // Get the selected text on the website.
+                    // https://stackoverflow.com/questions/5379120/get-the-highlighted-selected-text
+                    if (window.getSelection) {
+                        text = window.getSelection().toString()
+                    // Support for Internet Explorer 9 and below.
+                    } else if (document.selection && document.selection.type != "Control") {
+                        text = document.selection.createRange().text
+                    }
+
+                    // Copy the selected text.
+                    navigator.clipboard.writeText(text)
+
+                    // Fill the status pop-up with the text that has been copied.
+                    $("#status div p").innerHTML = `<img src="images/copy.png"> "${text}" gekopieerd.`
+
+                    // Show the status pop-up.
+                    $("#status").classList.add("show")
+
+                    // Hide the status pop-up after 3 seconds.
+                    setTimeout(function() {
+                        $("#status").classList.remove("show")
+                    }, 3000)
+                }
+
+                // Check if the word "plak" is said.
+                if (event.results[index][0].transcript == "plak") {
+                    navigator.clipboard.readText().then(text => {
+                        // Add the clipboard text to the HTML.
+                        $("#clipboard").innerText = text
+
+                        // Fill the status pop-up with the clipboard text.
+                        $("#status div p").innerHTML = `<img src="images/clipboard.png"> "${text}" geplakt.`
+
+                        // Show the status pop-up.
+                        $("#status").classList.add("show")
+
+                        // Hide the status pop-up after 3 seconds.
+                        setTimeout(function() {
+                            $("#status").classList.remove("show")
+                        }, 3000)
+                    })
+                }
             } else {
                 interim_transcript += event.results[index][0].transcript
             }
@@ -56,29 +116,21 @@ if ("webkitSpeechRecognition" in window) {
         $("#interim").innerHTML = interim_transcript
     }
 
-    // Start the speech recognition.
-    // speechRecognition.start()
+    $("#record").onclick = () => {
+        if (active == false) {
+            // Start the speech recognition.
+            speechRecognition.start()
 
-    $("#start").onclick = () => {
-        // Start the speech recognition.
-        speechRecognition.start()
+            // Set the state to active.
+            active = true
+        } else {
+            // Stop the speech recognition.
+            speechRecognition.stop()
+
+            // Set the state to inactive.
+            active = false
+        }
     }
-    
-    $("#stop").onclick = () => {
-        // Stop the speech recognition.
-        speechRecognition.stop()
-    }
-
-    
-
-    // navigator.permissions.query({name: "clipboard-write"}).then(result => {
-    //     if (result.state == "granted" || result.state == "prompt") {
-    //       /* write to the clipboard now */
-    //     }
-    //   });
-
-    // navigator.clipboard.readText().then(clipText =>
-        // $("#test").innerText = clipText);
 } else {
     console.log("Spraakherkenning niet beschikbaar.")
 }
